2cd00ee6d95237f66479c6d6baed34f34eb7dd4d
Nathan Tuttle <nathan.tuttle@gmail.com>
2013-05-06 19:04:21 -0500

    initial commit

---
+++ AccessCom3/AccessCom3/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace AccessCom3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            SpPerfChart.ChartPen chartPen1 = new SpPerfChart.ChartPen();
+            SpPerfChart.ChartPen chartPen2 = new SpPerfChart.ChartPen();
+            SpPerfChart.ChartPen chartPen3 = new SpPerfChart.ChartPen();
+            SpPerfChart.ChartPen chartPen4 = new SpPerfChart.ChartPen();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.performanceCounter1 = new System.Diagnostics.PerformanceCounter();
+            this.perfChart1 = new SpPerfChart.PerfChart();
+            ((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(37, 363);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(37, 393);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(138, 363);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // perfChart1
+            // 
+            this.perfChart1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.World);
+            this.perfChart1.Location = new System.Drawing.Point(251, 25);
+            this.perfChart1.Name = "perfChart1";
+            this.perfChart1.PerfChartStyle.AntiAliasing = true;
+            chartPen1.Color = System.Drawing.Color.Black;
+            chartPen1.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartPen1.Width = 1F;
+            this.perfChart1.PerfChartStyle.AvgLinePen = chartPen1;
+            this.perfChart1.PerfChartStyle.BackgroundColorBottom = System.Drawing.Color.Transparent;
+            this.perfChart1.PerfChartStyle.BackgroundColorTop = System.Drawing.Color.Transparent;
+            chartPen2.Color = System.Drawing.Color.Black;
+            chartPen2.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartPen2.Width = 1F;
+            this.perfChart1.PerfChartStyle.ChartLinePen = chartPen2;
+            chartPen3.Color = System.Drawing.Color.Black;
+            chartPen3.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartPen3.Width = 1F;
+            this.perfChart1.PerfChartStyle.HorizontalGridPen = chartPen3;
+            this.perfChart1.PerfChartStyle.ShowAverageLine = true;
+            this.perfChart1.PerfChartStyle.ShowHorizontalGridLines = true;
+            this.perfChart1.PerfChartStyle.ShowVerticalGridLines = true;
+            chartPen4.Color = System.Drawing.Color.Black;
+            chartPen4.DashStyle = System.Drawing.Drawing2D.DashStyle.Solid;
+            chartPen4.Width = 1F;
+            this.perfChart1.PerfChartStyle.VerticalGridPen = chartPen4;
+            this.perfChart1.ScaleMode = SpPerfChart.ScaleMode.Relative;
+            this.perfChart1.Size = new System.Drawing.Size(517, 376);
+            this.perfChart1.TabIndex = 4;
+            this.perfChart1.TimerInterval = 100;
+            this.perfChart1.TimerMode = SpPerfChart.TimerMode.Disabled;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(821, 430);
+            this.Controls.Add(this.perfChart1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            //((System.ComponentModel.ISupportInitialize)(this.performanceCounter1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Diagnostics.PerformanceCounter performanceCounter1;
+        private SpPerfChart.PerfChart perfChart1;
+    }
+}
+
---
+++ AccessCom3/AccessCom3/Form1.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.IO;
+using SpPerfChart;
+using System.Threading;
+
+namespace AccessCom3
+{
+    public partial class Form1 : Form
+    {
+        List<SensorData> sensorDataList = new List<SensorData>();
+        SpPerfChart.PerfChart chart = new PerfChart();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+          
+           
+            //chart1.DataSource = sensorDataList;
+
+            //chart1.DataBind();
+
+            serialPort1.Open();
+            timer1.Start();
+            RunTimer();
+           // serialPort1.Close();
+            this.Refresh();
+            
+
+        }
+        private void RunTimer()
+        {
+            backgroundWorker1.RunWorkerAsync(15);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //test theory
+            /*
+            [{ sensor :  262},{ output : 65}]
+            [{ sensor :  254},{ output : 63}]
+            [{ sensor :  240},{ output : 59}]
+             * */
+            var jsonString = "{\"sensor\":361,\"output\" :89}";// [{ \"sensor\" :  254},{ \"output\" : 63}],[{ \"sensor\" :  240},{ \"output\" : 59}]";
+
+            var json2 ="[{\"Id\": 1,\"Name\": \"HP Up\"},{\"Id\": 2,\"Name\": \"Regeneration\"}]";
+
+            var jsonPerson = "{\"age\":42,\"name\":\"John\"}";
+
+
+            var test = jsonHelper.From<SensorData>(jsonString);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            
+            
+            //this.Refresh();
+
+            perfChart1.TimerMode = TimerMode.Disabled;
+            perfChart1.ScaleMode = ScaleMode.Relative;
+            
+
+            //chart1.ChartAreas.Add("SesorData");
+            //chart1.ChartAreas.First().AxisX.Minimum = 0;
+            //chart1.ChartAreas.First().AxisX.Maximum = 255;
+            //chart1.ChartAreas.First().AxisX.Interval = 1;
+
+            //chart1.ChartAreas.First().AxisY.Minimum = 0;
+            //chart1.ChartAreas.First().AxisY.Maximum = 100;
+            //chart1.ChartAreas.First().AxisY.Interval = 1;
+            //chart1.Series.Add("output");
+            //chart1.Series.Add("snesor");
+            //chart1.Series["output"].Color = Color.Gray;
+            //chart1.Series["snesor"].Color = Color.Red;
+
+            //chart1.Series["output"].Points.AddXY(0, 0);
+            //chart1.Series["snesor"].Points.AddXY(0, 0);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int samplecounter = 0;
+           
+            {                
+                jsonHelper jsonHelper = new jsonHelper();
+                
+
+              //  do
+              //  {
+                    string json = serialPort1.ReadLine();
+                    try
+                    {
+                       // Console.WriteLine(json);
+                        var sensorData = jsonHelper.From<SensorData>(json);
+                        samplecounter++;
+                        sensorDataList.Add(sensorData);
+
+                    }
+                    catch (Exception) 
+                    {
+                        Console.WriteLine(string.Format(@"Could not interpret |{0}|",json));
+                    }
+
+             //   } while (samplecounter <= Convert.ToInt16(textBox1.Text));
+            }
+           
+
+            samplecounter = 0;
+            //RunTimer();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(100);
+
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            //perfChart1.Clear();
+            foreach (var sData in sensorDataList)
+            {
+                //chart1.Series["output"].Points.AddXY(samplecounter, sData.output);
+                //chart1.Series["snesor"].Points.AddXY(samplecounter, sData.sensor);\
+                //perfChart1.AddValue( sData.sensor);                
+                //chart.AddValue( sData.output);
+                System.Collections.BitArray bitarray = new System.Collections.BitArray(
+                    new byte[] 
+                        {
+                            (byte)sData.sensor 
+                        });
+
+
+
+                perfChart1.AddValue(sData.sensor);    
+                //foreach (bool bit in bitarray)
+                //{
+                //    var holdUp = bit ? 1 : 0;
+
+                //    perfChart1.AddValue(holdUp);    
+                //}
+                
+                
+
+                //Console.WriteLine(sData.sensor);
+                //samplecounter++;
+
+            }
+            
+            sensorDataList.Clear();
+
+            RunTimer();
+        }
+
+
+
+    }
+
+    [DataContract]
+    public class Person
+    {
+        public Person() { }
+
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public string Name { get; set; }
+    }
+
+    [DataContract]
+    public class SensorData
+    {
+        public SensorData() { }
+
+        [DataMember]
+        public int sensor { get; set; }
+
+        [DataMember]
+        public int output { get; set; }
+    }
+
+    [DataContract]
+    class Person2
+    {
+        [DataMember]
+        internal string name;
+
+        [DataMember]
+        internal int age;
+    }
+
+
+    public class jsonHelper
+    {
+        public static string To<T>(T obj)
+        {
+            string retVal = string.Empty;
+            System.Runtime.Serialization.Json.DataContractJsonSerializer serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(obj.GetType());
+            using (MemoryStream ms = new MemoryStream())
+            {
+                serializer.WriteObject(ms, obj);
+                retVal = Encoding.Default.GetString(ms.ToArray());
+            }
+            return retVal;
+        }
+
+        public static T From<T>(string json)
+        {
+            T obj = Activator.CreateInstance<T>();
+            using (MemoryStream ms = new MemoryStream(Encoding.Unicode.GetBytes(json)))
+            {
+                System.Runtime.Serialization.Json.DataContractJsonSerializer serializer = new System.Runtime.Serialization.Json.DataContractJsonSerializer(obj.GetType());
+                obj = (T)serializer.ReadObject(ms);
+            }
+
+            return obj;
+        }
+    }
+}
